35e3061c28cb8ef4136a4914ec2c42a49887350d
Axel <axosar@gmail.com>
2019-07-03 18:45:18 -0500

    Dynamic Routes
    Change Dynamic Route

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -59,7 +59,7 @@ class Posts extends Component {
         <section className="Posts">
           {posts}
         </section>
-        <Route path='/:id' exact component={FullPost} />
+        <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
       </div>
       
     )
